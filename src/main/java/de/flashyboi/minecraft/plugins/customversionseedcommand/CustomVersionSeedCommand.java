230fb1d3eb77ecb901d580d9503561b1d90be86a
flashbtw <johannes-kurt.reiter@freenet.de>
2021-10-29 15:10:03 +0200

    Log Tweaking at Startup

--- src/main/java/de/flashyboi/minecraft/plugins/customversionseedcommand/CustomVersionSeedCommand.java
+++ src/main/java/de/flashyboi/minecraft/plugins/customversionseedcommand/CustomVersionSeedCommand.java
@@ -7,16 +7,21 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
 
 public final class CustomVersionSeedCommand extends JavaPlugin {
     public static Plugin plugin;
+    public final Logger log = this.getLogger();
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+        log.info("Loading Events");
         Bukkit.getServer().getPluginManager().registerEvents(new VersionCommand(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new SeedCommand(), this);
+        log.info("Loaded Events successfully");
         this.saveDefaultConfig();
+        log.info("Enabled!");
     }
 
     @Override
