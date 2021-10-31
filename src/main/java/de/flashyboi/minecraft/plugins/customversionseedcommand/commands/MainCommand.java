06528e44453ac441290cfb4c51b3f59c2ce59e6b
flashbtw <johannes-kurt.reiter@freenet.de>
2021-10-31 21:32:55 +0100

    add reload

---
+++ src/main/java/de/flashyboi/minecraft/plugins/customversionseedcommand/commands/MainCommand.java
@@ -0,0 +1,49 @@
+package de.flashyboi.minecraft.plugins.customversionseedcommand.commands;
+
+import de.flashyboi.minecraft.plugins.customversionseedcommand.CustomVersionSeedCommand;
+import de.flashyboi.minecraft.plugins.customversionseedcommand.staticvar.ConfigVariables;
+import de.flashyboi.minecraft.plugins.customversionseedcommand.toolbox.HexCodeFormatter;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!(sender instanceof Player)) {
+                return false;
+            }
+            if (!(sender.hasPermission("CustomVersionSeedCommand.reload"))) {
+                String no_perms_for_reload = CustomVersionSeedCommand.plugin.getConfig().getString(ConfigVariables.NO_PERMISSION_RELOAD);
+                HexCodeFormatter message = new HexCodeFormatter(no_perms_for_reload);
+                String msg = message.hexCodeFormatter();
+                sender.sendMessage(msg);
+                return false;
+            }
+            CustomVersionSeedCommand.plugin.reloadConfig();
+            return true;
+        }
+        if (args.length > 1){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        List<String> subcommands = new ArrayList<>();
+        if(args.length == 1) {
+            subcommands.add("reload");
+            return subcommands;
+        }
+        return null;
+    }
+}
--- src/main/java/de/flashyboi/minecraft/plugins/customversionseedcommand/commands/SeedCommand.java
+++ src/main/java/de/flashyboi/minecraft/plugins/customversionseedcommand/commands/SeedCommand.java
@@ -2,8 +2,8 @@ package de.flashyboi.minecraft.plugins.customversionseedcommand.commands;
 
 import de.flashyboi.minecraft.plugins.customversionseedcommand.CustomVersionSeedCommand;
 import de.flashyboi.minecraft.plugins.customversionseedcommand.staticvar.ConfigVariables;
+import de.flashyboi.minecraft.plugins.customversionseedcommand.toolbox.HexCodeFormatter;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,15 +12,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SeedCommand implements Listener {
 
     private static final String ORIGINAL_SEED = "/minecraft:seed";
-    private static final String HEX_CODE_REGEX = "&#[a-fA-F0-9]{6}";
-
-    private final Pattern pattern = Pattern.compile(HEX_CODE_REGEX);
 
     private final FileConfiguration cfg = CustomVersionSeedCommand.plugin.getConfig();
     private final List<String> all_lines = cfg.getConfigurationSection(ConfigVariables.SEED_SECTION).getStringList(ConfigVariables.STRING_LIST_MESSAGE);
@@ -36,24 +31,13 @@ public class SeedCommand implements Listener {
                 e.setMessage(ORIGINAL_SEED);
             } else {
                 for(String one_line : all_lines) {
-                    one_line = hexCodeFormatter(one_line);
-                    player.sendMessage(one_line);
+                    HexCodeFormatter message = new HexCodeFormatter(one_line);
+                    String msg = message.hexCodeFormatter();
+                    player.sendMessage(msg);
                     e.setCancelled(true);
                 }
             }
-        } else {
-            return;
         }
-    }
-    private String hexCodeFormatter(String msg) {
-        Matcher match = pattern.matcher(msg);
-        while(match.find()) {
-            String color = msg.substring(match.start(), match.end());
-            color = color.replace("&","");
-            msg = msg.replace("&"+color, ChatColor.of(color) + "");
-            match = pattern.matcher(msg);
-        }
-        return msg;
     }
 }
 
